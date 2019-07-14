30bed778853371fcee600b2deb6c4c5a92732ac8
Patrick Geiger <patrickjgeiger@gmail.com>
2019-07-14 15:52:42 -0400

    filled out NumberGame component and began on Score

--- src/components/NumberGame.js
+++ src/components/NumberGame.js
@@ -1,1 +1,45 @@
 import React, { Component } from "react";
+
+class NumberGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correctAnswers: 0,
+      totalQuestions: 0
+    };
+  }
+
+  handleCorrectAnswer() {
+    this.setState(currentState => {
+      return {
+        correctAnswers: currentState.correctAnswers + 1,
+        totalQuestions: currentState.totalQuestions + 1
+      };
+    });
+  }
+
+  handleIncorrectAnswer() {
+    this.setState(currentState => {
+      return {
+        totalQuestions: currentState.totalQuestions + 1
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Score
+          correctAnswers={this.state.correctAnswers}
+          totalQuestions={this.state.totalQuestions}
+        />
+        <Equation
+          onCorrectAnswer={this.handleCorrectAnswer}
+          onIncorrectAnswer={this.handleIncorrectAnswer}
+        />
+      </div>
+    );
+  }
+}
+
+export default NumberGame;
